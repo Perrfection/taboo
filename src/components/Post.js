973a9322dc3646807719b4c9beacbc914737eb90
Perrfection <peterkinp123@gmail.com>
2022-07-07 12:30:47 -0400

    edit image

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { db } from "../../firebase";
+import Image from "next/Image";
 
 function Post( {id, title, description, imageUrl, pageUrl }) {
   const deletePosts = (e) => {
@@ -16,6 +17,7 @@ function Post( {id, title, description, imageUrl, pageUrl }) {
             src={imageUrl}
             alt=""
             className="postImages"
+            layout="fill"
           />
           <a href={pageUrl} className="postLink">
             {title} {id}
@@ -24,7 +26,7 @@ function Post( {id, title, description, imageUrl, pageUrl }) {
             <h2>{description}</h2>
           </div>
           <div>
-            <button className="b1" onClick={deletePosts}>Take Quiz </button>
+            <button className="b1" >Take Quiz </button>
             <button className="b1" onClick={deletePosts}>Delete </button>
           </div>
         </article>
